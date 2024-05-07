63b9c785fcf7b2b720d10f945401e8f642316065
nazyri <mohachevezsaborio@gmail.com>
2024-05-07 15:33:39 -0600

    botones

--- portafolio.js
+++ portafolio.js
@@ -8,36 +8,10 @@ const btnRegistro = document.getElementById("Registrarme")
 const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 
-try {
-    btnRegistro.addEventListener("click",e=>{
-        e.preventDefault()
-        let warnings = ""
-        let entrar = false
-        parrafo.innerHTML = ""
-        if (nombres.value.length <4){
-          parrafo.innerHTML += 'El nombre no es valido <br>'
-          entrar=true
-          console.log("dsad")
-        }
-        console.log(regexEmail.test(correo.value))
-        if(!regexEmail.test(correo.value)){
-          warnings += 'El correo no es valido <br>'
-          entrar=true
-        }
-        if(password.value.length <8){
-            warnings += 'La contraseña no es valido <br>'
-            entrar=true
-            console.log("dsad")
-        }
-        if(entrar){
-            parrafo.innerHTML = warnings
-        }else{
-            parrafo.innerHTML = "enviado"
-        }
+
         window.location.href="r.html"
-    })
     
-} catch (error) {
+ .catch (error) {
     console.error("El error fue ",error)    
 }
 
