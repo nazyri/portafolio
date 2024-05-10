78d92ef42a4ee76d008f23a5548f0d27d5d829a9
nazyri <mohachevezsaborio@gmail.com>
2024-05-10 15:51:35 -0600

    estilos

--- iniciarsecion.js
+++ iniciarsecion.js
@@ -1,19 +1,29 @@
 let nombreUsuario = document.getElementById("nombres")
 let gmail = document.getElementById("correo")
 let contraseña = document.getElementById("password")
+let boton = document.getElementById("iniciar")
 
-function guardarDatos() {
-    localStorage.setItem("nombres",nombreUsuario.value)
-    localStorage.setItem("correo",correo.value)
-    localStorage.setItem("password",contraseña.value)
-}
+iniciar.addEventListener("click",() =>{
+    const User = JSON.parse(localStorage.getItem("nombres"))
+    console.log(User[0].correo)
+    console.log(gmail.value)
+    if (gmail.value == User[0].correo && contraseña.value==User[0].password) {
+    
+    }
+    
+    if (validacionDeUsuario) {
+        alert("Coinciden")
+        
+    }else {
+        alert("Incorrectos")
+    }
+    
+})
 
 
 
 
-
-
-//Guardar datos//
-localStorage.setItem ("nombres".value)
-localStorage.setItem("correo".value)
-localStorage.setItem("password".value)
+//Guardar datos
+//localStorage.setItem ("nombres".value)
+//localStorage.setItem("correo".value)
+//localStorage.setItem("password".value)//
--- portafolio.js
+++ portafolio.js
@@ -1,21 +1,90 @@
-let form = document.getElementById("form")
+
 let usuario = document.getElementById("nombres")
 let primerApellido = document.getElementById("apellido")
 let gmail = document.getElementById("correo")
 let contraseña = document.getElementById("password")
+let boton = document.getElementById("resgistrarme")
 
-form.addEventListener("registar",e =>{
+let credenciales = JSON.parse(localStorage.getItem("nombres"))||[]
+
+function  usuario2() {
+    
+    let datos = {
+        
+        nombres: usuario.value,
+        apellido: primerApellido.value,
+        correo: gmail.value,
+        password: boton.value,
+    }
+    
+
+    credenciales.push(datos)
+    localStorage.setItem("nombres", JSON.stringify(usuario2))
+}
+
+contraseña.addEventListener("click", function () {
+    usuario2()
+})
+
+let correoLocal = localStorage.getItem ("correo")
+let apellidoLocal = localStorage.getItem ("apellido")
+let contraseñaLocal = localStorage.getItem ("password")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*form.addEventListener("registar",e =>{
     e.preventDefault();
     checkInputs();
 });
 
 function checkInputs() {
-    const usuarioValue = usuario.nodeValue.trim();
+    const usuariovalue = usuario.value.trim();
     const primerApellidovalue = primerApellido.value.trim();
     const gmailvalue = gmail.value.trim();
     const contraseñavalue = contraseña.value.trim();
 
-    if(usuarioValue === ''){
+    if(usuariovalue === ''){
         setErrorfor()
     }
 }
@@ -36,8 +105,8 @@ function guardarDatos() {
     localStorage.setItem("nombres",nombreUsuario.value)
     localStorage.setItem("apellido",primerApellido.value)
     localStorage.setItem("correo",correo.value)
-    localStorage.setItem("password",contraseña.value)
-}
+    localStorage.setItem("password",contraseña.value)*/
+
 
 
 
