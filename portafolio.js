0d13add8017e3b76cfb5c3fad5fe9d5cd82dfe74
nazyri <mohachevezsaborio@gmail.com>
2024-05-09 21:08:12 -0600

    estilos

--- portafolio.js
+++ portafolio.js
@@ -1,7 +1,36 @@
-let nombreUsuario = document.getElementById("nombres")
+let form = document.getElementById("form")
+let usuario = document.getElementById("nombres")
 let primerApellido = document.getElementById("apellido")
 let gmail = document.getElementById("correo")
 let contraseña = document.getElementById("password")
+
+form.addEventListener("registar",e =>{
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    const usuarioValue = usuario.nodeValue.trim();
+    const primerApellidovalue = primerApellido.value.trim();
+    const gmailvalue = gmail.value.trim();
+    const contraseñavalue = contraseña.value.trim();
+
+    if(usuarioValue === ''){
+        setErrorfor()
+    }
+}
+
+function setErrorfor(input, massage) {
+    const formCostrol = input.parentElement;
+    const small = formCostrol.querySelector('small');
+    formCostrol.classNa
+}
+
+
+
+
+
+
 
 function guardarDatos() {
     localStorage.setItem("nombres",nombreUsuario.value)
